2ca05eba17c3b98f18ac4fd600e7ef6012c831c2
lhelheo <leonardo.villalva@academico.ufs.br>
2024-06-01 01:37:17 -0300

    testing layouts

--- src/app/(plataform)/criar/page.tsx
+++ src/app/(plataform)/criar/page.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 
 export default function CriarReuniao() {
     const outletContent = (
-      <div>
-        <h1>Criar Reunião</h1>
-      </div>
+      <>
+        <div className='p-5'>
+            <h1 className='text-[#00694e] text-3xl'>Nova Reunião</h1>
+            <div className='text-[#3d3d3d] w-full h-full'>
+                <p>Link da Reunião</p>
+            </div>
+        </div>
+      </>
     );
   
     return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,64 +11,65 @@ export default function Home( {outletContent} : HomeProps) {
   return (
     <>
       <div className="grid grid-cols-6">
-        <div className="bg-[#133601] h-screen p-4">
-          <p className="flex justify-center text-white font-semibold text-2xl">Banese</p>
+        <div className="shadow-2xl h-screen p-4">
+          <p className="flex justify-center text-[#00694e] font-semibold text-2xl">Banese</p>
           <div className="text-white font-light flex flex-col justify-center items-center pt-5">
-            <p className="text-[22px]">RCU</p>
-            <p className="text-[16px]">REUNIÃO E CRIAÇÃO DE US</p>
+            <p className="text-[22px] text-[#00694e]">RCU</p>
+            <p className="text-[16px] text-[#00694e] ">REUNIÃO E CRIAÇÃO DE US</p>
           </div>
-          <div className="flex flex-col p-6 gap-6">
+          <div className="flex flex-col p-6 gap-6 h-[820px]">
             <div className="flex flex-col gap-6 justify-between h-full">
               
               <div>
                 <p 
-                  className="bg-white text-[#133601] p-3 rounded-md flex my-3 cursor-pointer"
+                  className="bg-[#00694e] text-[#133601] p-3 rounded-md flex my-3 cursor-pointer gap-2"
                   onClick={() => router.push('/criar')}  
                 >
-                  <Plus />
-                  Nova Reunião
+                  <Plus  className="text-white"/>
+                  <span className="text-white">Nova Reunião</span>
                 </p>
                 <p 
-                  className="bg-white text-[#133601] p-3 rounded-md flex my-3 cursor-pointer"
+                  className="bg-[#00694e] text-[#133601] p-3 rounded-md flex my-3 cursor-pointer gap-2"
                   onClick={() => console.log('a')}  
                 >
-                  <Upload />
-                  Upload de Reunião
+                  <Upload className="text-white"/>
+                  <span className="text-white">Upload de Reunião</span>
                 </p>
                   
                 <p 
-                  className="bg-white text-[#133601] p-3 rounded-md flex my-3 cursor-pointer"
+                  className="bg-[#00694e] text-[#133601] p-3 rounded-md flex my-3 cursor-pointer gap-2"
                   onClick={() => console.log('a')}
                 >
-                  <Calendar />
-                  Agendar Reunião
+                  <Calendar className="text-white"/>
+                  <span className="text-white">Agendar Reunião</span>
                 </p>
               </div>
               
               <div>
                 <span 
-                  className="bg-white text-[#133601] p-3 rounded-md flex my-3 cursor-pointer"
+                  className="bg-[#00694e] text-[#133601] p-3 rounded-md flex my-3 cursor-pointer gap-2"
                   onClick={() => console.log('a')}
                 >
-                  <Clock />
-                  Histórico de Reunião
+                  <Clock className="text-white"/>
+                  <p className="text-white">Histórico de Reunião</p>
                 </span>
-                <span 
-                  className="bg-white text-[#133601] p-3 rounded-md flex my-3 cursor-pointer"
+                <p 
+                  className="bg-[#00694e] text-[#133601] p-3 rounded-md flex my-3 cursor-pointer gap-2"
                   onClick={() => console.log('a')}  
                 >
-                  <Settings />
-                  Configurações
-                </span>
+                  <Settings className="text-white"/>
+                  <span className="text-white">Configurações</span>
+                </p>
               </div>
 
               </div>
           </div>
         </div>
      
-        <div className="">
-          {outletContent}
-        </div> 
+        <div className="mx-auto max-w-full overflow-x-hidden whitespace-no-wrap">
+            {outletContent}
+        </div>
+
       </div>
     </>
   );
